888b12ed1b3b238716f1f90f4066e124fb98d1b0
VoN <developerjesus@hotmail.com>
2021-02-27 00:52:10 -0600

    feat: :sparkles: Add EffectHook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import StateHook from './hooks/StateHook';
 import ExampleWithManyStates from './hooks/ExampleWithManyStates';
+import EffectHook from './hooks/EffectHook';
 
 class App extends Component {
   render() {
     return (
-      <ExampleWithManyStates/>
+      <EffectHook/>
     );
   }
 }
